386d7ead97b0e4ee598796a1dc213f363157a8cd
11_pochta@bk.ru <11_pochta@bk.ru>
2021-08-14 12:38:41 +0600

    4.3.6. Math.abs [#373616 #189361] Добавил ограничение для координат сверху (не дожно быть больше 7)

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,7 +2,8 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        if (x1 >= 0 && y1 >= 0 && x2 >= 0 && y2 >= 0) {
+        if (x1 >= 0 && y1 >= 0 && x2 >= 0 && y2 >= 0
+            && x1 < 8 && y1 < 8 && x2 < 8 && y2 < 8) {
             int countMove = Math.abs(x1 - x2) == Math.abs(y1 - y2) ? Math.abs(x1 - x2) : 0;
             return countMove;
         }
