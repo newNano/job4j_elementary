6f815d1cd5a40ae8ab502a6a46ea2a804443c0bb
11_pochta@bk.ru <11_pochta@bk.ru>
2021-08-15 18:18:51 +0600

    5.9. Крест в псевдографике. [#177182 #189797] Переделано. Метод main отправлен в самый низ. Изменено условие для boolean right

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -1,20 +1,11 @@
 package ru.job4j.loop;
 
 public class Slash {
-    public static void main(String[] args) {
-        System.out.println("Draw by 3");
-        draw(3);
-        System.out.println("Draw by 5");
-        draw(5);
-    }
-
-    //
     public static void draw(int size) {
-        int indexRight = size - 1;
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
                 boolean left = row == cell;
-                boolean right = cell == indexRight;
+                boolean right = row + cell == size - 1;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
@@ -23,9 +14,15 @@ public class Slash {
                     System.out.print(" ");
                 }
             }
-            indexRight--;
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
     }
 }
