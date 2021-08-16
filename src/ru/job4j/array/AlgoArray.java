2ae73fedad2d10d427672af77fef74b69b3c79f6
11_pochta@bk.ru <11_pochta@bk.ru>
2021-08-16 16:16:01 +0600

    6.2. Упорядочить массив. [#4441 #190111] Перестановка элементов массива с помощью индексов

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -3,15 +3,18 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
+
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
 
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
