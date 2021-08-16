5726ed66a962c40347995120ddfcaaed6cdc754e
11_pochta@bk.ru <11_pochta@bk.ru>
2021-08-16 17:14:12 +0600

    6.4.2. Слово заканчивается на ... [#156317 #190213] Переписал метод endsWith, убрав статический метод endsWith класса String, и добавил условие, которое проверяет элементы массивов с конца

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,10 +2,12 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        String wordStr = String.valueOf(word);
-        String postStr = String.valueOf(post);
-        boolean result = wordStr.endsWith(postStr);
-        /* проверить. что массив word имеет последние элементы одинаковые с post */
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
+                return false;
+            }
+        }
         return result;
     }
 }
